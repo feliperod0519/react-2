0cc01d83ef0710defe202835cc449dccf47d5ee7
feliperod0519@hotmail.com <juan_andres_rodriguez_0519@hotmail.com>
2023-10-05 22:15:34 -0400

    initial commit ja

---
+++ components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Sept 10th 1973</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$100</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
